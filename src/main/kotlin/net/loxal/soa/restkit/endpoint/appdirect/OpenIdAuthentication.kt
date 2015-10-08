5e063c5e35deeb4ed013c4d2b7536013d20f7ea5
Alex <alexander.orlov@loxal.net>
2015-10-08 02:30:34 +0200

    replace openid4java with the Spring based wrapper to simplify implementation

--- src/main/kotlin/net/loxal/soa/restkit/endpoint/appdirect/OpenIdAuthentication.kt
+++ src/main/kotlin/net/loxal/soa/restkit/endpoint/appdirect/OpenIdAuthentication.kt
@@ -7,20 +7,17 @@ package net.loxal.soa.restkit.endpoint.appdirect
 import net.loxal.soa.restkit.endpoint.Endpoint
 import net.loxal.soa.restkit.endpoint.appdirect.dto.OpenIdInfo
 import net.loxal.soa.restkit.model.common.ErrorMessage
-import org.openid4java.consumer.ConsumerManager
-import org.openid4java.consumer.VerificationResult
-import org.openid4java.discovery.DiscoveryInformation
-import org.openid4java.message.AuthRequest
-import org.openid4java.message.Parameter
-import org.openid4java.message.ParameterList
+import org.springframework.security.openid.OpenID4JavaConsumer
+import org.springframework.security.openid.OpenIDAuthenticationToken
+import org.springframework.security.openid.OpenIDConsumer
 import java.net.URI
 import java.net.URL
+import javax.servlet.http.HttpServletRequest
 import javax.validation.constraints.NotNull
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.QueryParam
 import javax.ws.rs.container.AsyncResponse
-import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.Suspended
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
@@ -30,26 +27,17 @@ class OpenIdAuthentication : Endpoint() {
 
     @GET
     fun verify(
-            @Context req: ContainerRequestContext,
+            @Context req: HttpServletRequest,
             @Suspended asyncResponse: AsyncResponse
     ) {
-        val responseParameters = ParameterList()
-        req.uriInfo.queryParameters.forEach {
-            responseParameters.set(Parameter(it.key, it.value.first()))
-        }
+        val token: OpenIDAuthenticationToken = openIdConsumer.endConsumption(req)
 
-        val verification: VerificationResult = openIdConsumer.verify(
-                req.uriInfo.requestUri.toString(),
-                responseParameters,
-                endpointAssociation
-        )
-
-        Endpoint.LOG.info("openid.return_to: ${req.uriInfo.queryParameters["openid.return_to"]}")
-        if (verification.verifiedId?.identifier == null) {
-            asyncResponse.resume(Response.status(Response.Status.UNAUTHORIZED).entity(ErrorMessage(status = Response.Status.UNAUTHORIZED)).build())
+        Endpoint.LOG.info("openid.return_to: ${req.getParameter("openid.return_to")}")
+        if (token.identityUrl == null) {
+            asyncResponse.resume(Response.status(Response.Status.UNAUTHORIZED).entity(ErrorMessage(Response.Status.UNAUTHORIZED)).build())
         } else {
-            Endpoint.LOG.info("requestUri: ${req.uriInfo.requestUri}")
-            val redirection = "${req.uriInfo.baseUri}$clientRedirectionPath?openid.id=${verification.verifiedId.identifier}"
+            Endpoint.LOG.info("requestUri: ${req.requestURI}")
+            val redirection = "${req.requestURL}/../$clientRedirectionPath?openid.id=${token.identityUrl}"
             asyncResponse.resume(Response.temporaryRedirect(URI.create(redirection)).build())
         }
     }
@@ -57,26 +45,22 @@ class OpenIdAuthentication : Endpoint() {
     @Path("openid")
     @GET
     fun authenticate(
-            @QueryParam("url") url: URL?,
+            @NotNull @QueryParam("url") url: URL?,
             @NotNull @QueryParam("returnToUrl") returnToUrl: URL?,
+            @Context req: HttpServletRequest,
             @Suspended asyncResponse: AsyncResponse
     ) {
-        val idDiscoveries = openIdConsumer.discover(url.toString())
-        endpointAssociation = openIdConsumer.associate(idDiscoveries)
+        val signInUrl: URL = URL(openIdConsumer.beginConsumption(req, url.toString(), returnToUrl.toString(), returnToUrl.toString()))
 
-        val authReq: AuthRequest = openIdConsumer.authenticate(endpointAssociation, returnToUrl.toString())
-
-        val signInUrl: URL = URL(authReq.getDestinationUrl(true))
-        val openIdInfo = OpenIdInfo(url = url, returnToUrl = returnToUrl, signInUrl = signInUrl)
         Endpoint.LOG.info("signInUrl: $signInUrl")
+        val openIdInfo = OpenIdInfo(url = url, returnToUrl = returnToUrl, signInUrl = signInUrl)
         asyncResponse.resume(Response.ok(openIdInfo).location(signInUrl.toURI()).build())
     }
 
     companion object {
         const val RESOURCE_PATH = "authentication"
 
-        private val clientRedirectionPath = "play/ground.html"
-        private val openIdConsumer: ConsumerManager = ConsumerManager()
-        private lateinit var endpointAssociation: DiscoveryInformation
+        private const val clientRedirectionPath = "play/ground.html"
+        private val openIdConsumer: OpenIDConsumer = OpenID4JavaConsumer()
     }
 }
